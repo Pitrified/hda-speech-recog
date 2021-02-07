9bd7c2c3290959e3f1b6a761a82cb899be13b5ac
Pitrified <nobili.pietro@gmail.com>
2021-02-07 02:20:36 +0100

    Add prepare_partitions

--- audio_generator.py
+++ audio_generator.py
@@ -19,18 +19,22 @@ class AudioGenerator(Sequence):
     def __init__(
         self,
         list_IDs: ty.List[str],
-        ids2labels: ty.Dict[str, str],
+        ids2labels: ty.Dict[str, ty.Tuple[str, str]],
         data_split_path: Path,
         batch_size=32,
-        dim=(32, 32, 32),
+        dim=(64, 64),
         n_channels=1,
         n_classes=10,
         shuffle=True,
     ) -> None:
         """TODO: what is __init__ doing?
 
+        https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+
         partition = {'train': ['id-1', 'id-2', 'id-3'], 'validation': ['id-4']}
-        ids2labels = {'id-1': 0, 'id-2': 1, 'id-3': 2, 'id-4': 1}
+        ids2labels = {'id-1': ('happy', '12543_nohash_1'), 'id-2': ('wow',
+        '92543_nohash_1'), 'id-3': ('learn', '12843_nohash_1'), 'id-4': ('happy',
+        '18240_nohash_1')}
 
         will be loaded by np.load('data/' + ID + '.npy')
         will be called like train_gen = AudioGenerator(partition['train'], ids2labels, **params)
@@ -75,14 +79,14 @@ class AudioGenerator(Sequence):
         self.label_encoder = LabelEncoder()
         self.label_encoder.fit(self.y_labels)
 
+        # the root folder of the processed dataset
         self.data_split_path = data_split_path
-        self.name_template = "{}.npy"
 
-    def __len__(self):
+    def __len__(self) -> int:
         "Denotes the number of batches per epoch"
         return int(np.floor(len(self.list_IDs) / self.batch_size))
 
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> ty.Tuple[np.ndarray, np.ndarray]:
         "Generate one batch of data"
         # Generate indexes of the batch
         indexes = self.indexes[index * self.batch_size : (index + 1) * self.batch_size]
@@ -95,27 +99,27 @@ class AudioGenerator(Sequence):
 
         return X, y
 
-    def on_epoch_end(self):
+    def on_epoch_end(self) -> None:
         "Updates indexes after each epoch"
         self.indexes = np.arange(len(self.list_IDs))
         if self.shuffle is True:
             np.random.shuffle(self.indexes)
 
-    def __data_generation(self, list_IDs_temp):
+    def __data_generation(self, list_IDs_temp) -> ty.Tuple[np.ndarray, np.ndarray]:
         """Generates data containing batch_size samples
 
         X : (n_samples, *dim, n_channels)
         """
         # Initialization
         X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = []
+        y: ty.List[str] = []
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
-            file_path = self.data_split_path / self.name_template.format(ID)
+            word, wav_stem = self.ids2labels[ID]
+            file_path = self.data_split_path / word / f"{wav_stem}.npy"
             X[i] = np.load(file_path)
-
-            y.append(self.ids2labels[ID])
+            y.append(word)
 
         y_hot = self.label_encoder.fit(y)
         return X, y_hot
@@ -130,7 +134,7 @@ def parse_arguments() -> argparse.Namespace:
         "--preprocess_type",
         type=str,
         default="preprocess_split",
-        choices=["preprocess_split"],
+        choices=["preprocess_split", "test_audio_generator"],
         help="Which preprocess to perform",
     )
 
@@ -180,7 +184,7 @@ def setup_env() -> argparse.Namespace:
 
 
 def compute_folder_spec(
-    word_in_path,
+    word_in_folder,
     word_out_folder,
     dataset_name,
     force_preprocess,
@@ -189,7 +193,7 @@ def compute_folder_spec(
 ) -> None:
     """TODO: what is compute_folder_spec doing?"""
     # extract all the wavs
-    all_wavs = list(word_in_path.iterdir())
+    all_wavs = list(word_in_folder.iterdir())
 
     for wav_path in all_wavs:
         wav_stem = wav_path.stem
@@ -243,12 +247,12 @@ def preprocess_split(
             word_out_folder.mkdir(parents=True, exist_ok=True)
 
         # the input folder path
-        word_in_path = data_raw_path / word
+        word_in_folder = data_raw_path / word
 
         # build the arguments of the functions you will run
         arguments.append(
             (
-                word_in_path,
+                word_in_folder,
                 word_out_folder,
                 dataset_name,
                 force_preprocess,
@@ -264,6 +268,68 @@ def preprocess_split(
         p.get()
 
 
+def prepare_partitions(words_type: str) -> None:
+    """TODO: what is prepare_partitions doing?"""
+    logg = logging.getLogger(f"c.{__name__}.prepare_partitions")
+    # logg.setLevel("INFO")
+    logg.debug("Start prepare_partitions")
+
+    data_raw_path = Path("data_raw")
+
+    # list of file names for validation
+    validation_path = data_raw_path / "validation_list.txt"
+    validation_names = []
+    with validation_path.open() as fvp:
+        for line in fvp:
+            validation_names.append(line.strip())
+
+    # list of file names for testing
+    testing_path = data_raw_path / "testing_list.txt"
+    testing_names = []
+    with testing_path.open() as fvp:
+        for line in fvp:
+            testing_names.append(line.strip())
+
+    partition: ty.Dict[str, ty.List[str]] = {
+        "training": [],
+        "validation": [],
+        "testing": [],
+    }
+    ids2labels: ty.Dict[str, ty.Tuple[str, str]] = {}
+
+    words = words_types[words_type]
+    for word in words:
+
+        # the input folder path
+        word_in_folder = data_raw_path / word
+
+        # go through all the words in this folder
+        all_wavs = list(word_in_folder.iterdir())
+        for wav_path in tqdm(all_wavs):
+            wav_stem = wav_path.stem
+            wav_name = f"{word}/{wav_path.name}"
+
+            if wav_name in validation_names:
+                which = "validation"
+            elif wav_name in testing_names:
+                which = "testing"
+            else:
+                which = "training"
+
+            ID = wav_name
+            partition[which].append(ID)
+            ids2labels[ID] = (word, wav_stem)
+
+
+def test_audio_generator(which_dataset: str, words_type: str) -> None:
+    """TODO: what is test_audio_generator doing?"""
+    logg = logging.getLogger(f"c.{__name__}.test_audio_generator")
+    # logg.setLevel("INFO")
+    logg.debug("Start test_audio_generator")
+
+    prepare_partitions(words_type)
+
+
 def run_audio_generator(args: argparse.Namespace) -> None:
     """TODO: What is audio_generator doing?"""
     logg = logging.getLogger(f"c.{__name__}.run_audio_generator")
@@ -276,6 +342,8 @@ def run_audio_generator(args: argparse.Namespace) -> None:
 
     if preprocess_type == "preprocess_split":
         preprocess_split(which_dataset, words_type, force_preprocess)
+    elif preprocess_type == "test_audio_generator":
+        test_audio_generator(which_dataset, words_type)
 
 
 if __name__ == "__main__":
