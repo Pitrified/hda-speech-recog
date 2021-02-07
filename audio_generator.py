026773d4cf3af270457772a41eaa5aef6505a954
Pitrified <nobili.pietro@gmail.com>
2021-02-07 13:05:36 +0100

    Add working example of AudioGenerator use

--- audio_generator.py
+++ audio_generator.py
@@ -1,16 +1,23 @@
 from pathlib import Path
+from sklearn.metrics import confusion_matrix  # type: ignore
 from sklearn.preprocessing import LabelEncoder  # type: ignore
 from tensorflow.keras.utils import Sequence  # type: ignore
+from tensorflow.keras.utils import to_categorical  # type: ignore
 from tqdm import tqdm  # type: ignore
 import argparse
 import logging
+import matplotlib.pyplot as plt  # type: ignore
 import multiprocessing as mp
 import numpy as np  # type: ignore
+import tensorflow as tf  # type: ignore
 import typing as ty
 
+from models import CNNmodel
+from plot_utils import plot_confusion_matrix
 from preprocess_data import get_spec_dict
 from preprocess_data import wav2mel
 from preprocess_data import wav2mfcc
+from utils import analyze_confusion
 from utils import setup_logger
 from utils import words_types
 
@@ -20,11 +27,10 @@ class AudioGenerator(Sequence):
         self,
         list_IDs: ty.List[str],
         ids2labels: ty.Dict[str, ty.Tuple[str, str]],
-        data_split_path: Path,
+        label_names: ty.List[str],
+        data_split_paths: ty.List[Path],
         batch_size=32,
         dim=(64, 64),
-        n_channels=1,
-        n_classes=10,
         shuffle=True,
     ) -> None:
         """TODO: what is __init__ doing?
@@ -32,55 +38,39 @@ class AudioGenerator(Sequence):
         https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 
         partition = {'train': ['id-1', 'id-2', 'id-3'], 'validation': ['id-4']}
-        ids2labels = {'id-1': ('happy', '12543_nohash_1'), 'id-2': ('wow',
-        '92543_nohash_1'), 'id-3': ('learn', '12843_nohash_1'), 'id-4': ('happy',
-        '18240_nohash_1')}
+        ids2labels = {'id-1': ('happy', '12543_nohash_1'), ...}
 
         will be loaded by np.load('data/' + ID + '.npy')
         will be called like train_gen = AudioGenerator(partition['train'], ids2labels, **params)
-
-
-        >>> lab = ['a', 'b', 'c', 'a']
-        >>> y = LabelEncoder().fit_transform(lab)
-        >>> y
-        array([0, 1, 2, 0])
-        >>> cat = to_categorical(y)
-        >>> cat
-        array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.], [1., 0., 0.]], dtype=float32)
-
-        >>> le = LabelEncoder()
-        >>> le.fit(lab)
-        >>> le.classes_
-        array(['a', 'b', 'c'], dtype='<U1')
-        >>> ind = np.argmax(cat[0])
-        >>> ind
-        0
-        >>> le.inverse_transform([ind])
-        array(['a'], dtype='<U1')
-
-
         """
-        logg = logging.getLogger(f"c.{__name__}.__init__")
+        # logg = logging.getLogger(f"c.{__name__}.__init__")
         # logg.setLevel("INFO")
-        logg.debug("Start __init__")
+        # logg.debug("Start __init__")
 
-        self.dim = dim
-        self.batch_size = batch_size
-        self.ids2labels = ids2labels
+        # info on the data and labels
         self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
+        self.ids2labels = ids2labels
+        self.label_names = label_names
 
-        # y_labels: ['happy', 'learn', 'wow', 'visual']
-        self.y_labels = sorted(list(set(ids2labels[k] for k in ids2labels)))
-        logg.debug(f"self.y_labels: {self.y_labels}")
+        # a LabelEncoder to go from label name to index realiably
+        # then use to_categorical to get one hot encoded
+        # to reverse, get the indexes with argmax from the predictions
+        # and use inverse_transform
         self.label_encoder = LabelEncoder()
-        self.label_encoder.fit(self.y_labels)
+        self.label_encoder.fit(self.label_names)
 
         # the root folder of the processed dataset
-        self.data_split_path = data_split_path
+        self.data_split_paths = data_split_paths
+        # each dataset is a channel
+        self.n_channels = len(self.data_split_paths)
+
+        # info on the shape of the batch to generate
+        self.batch_size = batch_size
+        self.dim = dim
+        self.n_classes = len(self.label_names)
+
+        self.shuffle = shuffle
+        self.on_epoch_end()
 
     def __len__(self) -> int:
         "Denotes the number of batches per epoch"
@@ -116,13 +106,66 @@ class AudioGenerator(Sequence):
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
+
+            # extract path info for this word
             word, wav_stem = self.ids2labels[ID]
-            file_path = self.data_split_path / word / f"{wav_stem}.npy"
-            X[i] = np.load(file_path)
+
+            full_spec = np.empty((*self.dim, self.n_channels))
+            # print(f"full_spec.shape: {full_spec.shape}")
+            # print(f"full_spec[:, :, 0].shape: {full_spec[:, :, 0].shape}")
+
+            # load a spectrogram from each dataset
+            for j, data_split_path in enumerate(self.data_split_paths):
+                file_path = data_split_path / word / f"{wav_stem}.npy"
+                spec = np.load(file_path)
+                # print(f"spec.shape: {spec.shape}")
+                full_spec[:, :, j] = spec
+
+            X[i] = full_spec
             y.append(word)
 
-        y_hot = self.label_encoder.fit(y)
+        y_le = self.label_encoder.transform(y)
+        y_hot = to_categorical(y_le, num_classes=self.n_classes)
         return X, y_hot
+
+    def pred2labelnames(self, y_pred) -> ty.List[str]:
+        """TODO: what is pred2labelnames doing?
+
+        >>> lab = ['a', 'b', 'c', 'a']
+        >>> y = LabelEncoder().fit_transform(lab)
+        array([0, 1, 2, 0])
+        >>> cat = to_categorical(y)
+        array([[1., 0., 0.], [0., 1., 0.], [0., 0., 1.], [1., 0., 0.]], dtype=float32)
+        >>> le = LabelEncoder()
+        >>> le.fit(lab)
+        >>> le.classes_
+        array(['a', 'b', 'c'], dtype='<U1')
+        >>> ind = np.argmax([0.9, 0.04, 0.06])
+        0
+        >>> le.inverse_transform([ind])
+        array(['a'], dtype='<U1')
+        """
+        # logg = logging.getLogger(f"c.{__name__}.pred2labelnames")
+        # logg.setLevel("INFO")
+        # logg.debug("Start pred2labelnames")
+
+        y_ind = np.argmax(y_pred, axis=1)
+        y_lab = self.label_encoder.inverse_transform(y_ind)
+        return y_lab
+
+    def get_true_labels(self) -> ty.List[str]:
+        """TODO: what is get_true_labels doing?"""
+        # logg = logging.getLogger(f"c.{__name__}.get_true_labels")
+        # logg.setLevel("INFO")
+        # logg.debug("Start get_true_labels")
+
+        # if the data was shuffled follow that order
+        list_IDs_temp = [self.list_IDs[k] for k in self.indexes]
+
+        y_true = []
+        for ID in list_IDs_temp:
+            y_true.append(self.ids2labels[ID][0])
+        return y_true
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -209,9 +252,10 @@ def compute_folder_spec(
             log_spec = wav2mfcc(wav_path, spec_kwargs, p2d_kwargs)
         elif dataset_name.startswith("mel"):
             log_spec = wav2mel(wav_path, spec_kwargs, p2d_kwargs)
-        img_spec = log_spec.reshape((*log_spec.shape, 1))
 
-        np.save(spec_path, img_spec)
+        # img_spec = log_spec.reshape((*log_spec.shape, 1))
+        # np.save(spec_path, img_spec)
+        np.save(spec_path, log_spec)
 
 
 def preprocess_split(
@@ -268,7 +312,9 @@ def preprocess_split(
         p.get()
 
 
-def prepare_partitions(words_type: str) -> None:
+def prepare_partitions(
+    words_type: str,
+) -> ty.Tuple[ty.Dict[str, ty.List[str]], ty.Dict[str, ty.Tuple[str, str]]]:
     """TODO: what is prepare_partitions doing?"""
     logg = logging.getLogger(f"c.{__name__}.prepare_partitions")
     # logg.setLevel("INFO")
@@ -298,14 +344,14 @@ def prepare_partitions(words_type: str) -> None:
     ids2labels: ty.Dict[str, ty.Tuple[str, str]] = {}
 
     words = words_types[words_type]
-    for word in words:
+    logg.debug(f"Partinioning words: {words}")
+    for word in tqdm(words):
 
         # the input folder path
         word_in_folder = data_raw_path / word
 
         # go through all the words in this folder
-        all_wavs = list(word_in_folder.iterdir())
-        for wav_path in tqdm(all_wavs):
+        for wav_path in word_in_folder.iterdir():
             wav_stem = wav_path.stem
             wav_name = f"{word}/{wav_path.name}"
 
@@ -320,6 +366,8 @@ def prepare_partitions(words_type: str) -> None:
             partition[which].append(ID)
             ids2labels[ID] = (word, wav_stem)
 
+    return partition, ids2labels
+
 
 def test_audio_generator(which_dataset: str, words_type: str) -> None:
     """TODO: what is test_audio_generator doing?"""
@@ -327,7 +375,83 @@ def test_audio_generator(which_dataset: str, words_type: str) -> None:
     # logg.setLevel("INFO")
     logg.debug("Start test_audio_generator")
 
-    prepare_partitions(words_type)
+    partition, ids2labels = prepare_partitions(words_type)
+
+    for fold in partition:
+        logg.debug(f"partition[{fold}][:4]: {partition[fold][:4]}")
+
+    logg.debug(f"\nlen(ids2labels): {len(ids2labels)}")
+    for ID in ids2labels:
+        logg.debug(f"ids2labels[{ID}]: {ids2labels[ID]}")
+        break
+
+    words = words_types[words_type]
+    processed_folder = Path("data_split") / "mel04"
+    data_split_paths = [processed_folder]
+
+    params = {
+        "dim": (64, 64),
+        "batch_size": 32,
+        "shuffle": True,
+        "label_names": words,
+        "data_split_paths": data_split_paths,
+    }
+
+    training_generator = AudioGenerator(partition["training"], ids2labels, **params)
+    logg.debug(f"len(training_generator): {len(training_generator)}")
+
+    val_generator = AudioGenerator(partition["validation"], ids2labels, **params)
+    logg.debug(f"len(val_generator): {len(val_generator)}")
+
+    # do not shuffle the test data
+    params["shuffle"] = False
+    # do not batch it, no loss of stray data at the end
+    params["batch_size"] = 1
+    testing_generator = AudioGenerator(partition["testing"], ids2labels, **params)
+    logg.debug(f"len(testing_generator): {len(testing_generator)}")
+
+    X, y = training_generator[0]
+    logg.debug(f"X.shape: {X.shape} y.shape: {y.shape}")
+
+    model_param: ty.Dict[str, ty.Any] = {}
+    model_param["num_labels"] = len(words)
+    model_param["input_shape"] = (64, 64, 1)
+    model_param["base_dense_width"] = 32
+    model_param["base_filters"] = 20
+    model_param["dropouts"] = [0.03, 0.01]
+    model_param["kernel_sizes"] = [(5, 1), (3, 3), (3, 3)]
+    model_param["pool_sizes"] = [(2, 1), (2, 2), (2, 2)]
+    model = CNNmodel(**model_param)
+    # model.summary()
+
+    metrics = [
+        tf.keras.metrics.CategoricalAccuracy(),
+        tf.keras.metrics.Precision(),
+        tf.keras.metrics.Recall(),
+    ]
+    opt = tf.optimizers.Adam()
+    loss = tf.keras.losses.CategoricalCrossentropy()
+    model.compile(optimizer=opt, loss=loss, metrics=metrics)
+
+    EPOCH_NUM = 5
+    model.fit(training_generator, validation_data=val_generator, epochs=EPOCH_NUM)
+
+    eval_testing = model.evaluate(testing_generator)
+    for metrics_name, value in zip(model.metrics_names, eval_testing):
+        logg.debug(f"{metrics_name}: {value}")
+
+    y_pred = model.predict(testing_generator)
+    y_pred_labels = testing_generator.pred2labelnames(y_pred)
+    y_true = testing_generator.get_true_labels()
+
+    cm = confusion_matrix(y_true, y_pred_labels)
+    fscore = analyze_confusion(cm, words)
+    logg.debug(f"fscore: {fscore}")
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+    plot_confusion_matrix(cm, ax, "Test generator", words, fscore)
+    fig.tight_layout()
+    plt.show()
 
 
 def run_audio_generator(args: argparse.Namespace) -> None:
